3826becbe8a3019715a77af833abc4071740f749
DenisVS <denvsim@gmail.com>
2025-08-16 14:44:58 +0200

    ps scripts added

---
+++ remove_apps.ps1
@@ -0,0 +1,33 @@
+Get-AppxPackage *maps* | Remove-AppxPackage
+Get-AppxPackage *bing* | Remove-AppxPackage
+Get-AppxPackage *movies* | Remove-AppxPackage
+Get-AppxPackage *onedrive* | Remove-AppxPackage
+Get-AppxPackage *onenote* | Remove-AppxPackage
+Get-AppxPackage *office* | Remove-AppxPackage
+Get-AppxPackage *paint* | Remove-AppxPackage
+Get-AppxPackage *photos* | Remove-AppxPackage
+Get-AppxPackage *outlook* | Remove-AppxPackage
+Get-AppxPackage *skype* | Remove-AppxPackage
+Get-AppxPackage *sketch* | Remove-AppxPackage
+Get-AppxPackage *record* | Remove-AppxPackage
+Get-AppxPackage *webp* | Remove-AppxPackage
+Get-AppxPackage *webmedia* | Remove-AppxPackage
+Get-AppxPackage *heif* | Remove-AppxPackage
+Get-AppxPackage *Calendar* | Remove-AppxPackage
+Get-AppxPackage *MixedReality* | Remove-AppxPackage
+Get-AppxPackage *AppInstaller* | Remove-AppxPackage
+Get-AppxPackage *gethelp* | Remove-AppxPackage
+Get-AppxPackage *tips* | Remove-AppxPackage
+Get-AppxPackage *phonelink* | Remove-AppxPackage
+Get-AppxPackage *yourphone*   | Remove-AppxPackage
+
+Remove-appxpackage Microsoft.MicrosoftEdge.Stable_92.0.902.67_neutral__8wekyb3d8bbwe
+
+Get-AppxPackage *Calculator*   | Remove-AppxPackage
+Get-AppxPackage *Alarm*   | Remove-AppxPackage
+Get-AppxPackage *Groove*   | Remove-AppxPackage
+Get-AppxPackage *Mail*   | Remove-AppxPackage
+Get-AppxPackage *Movies*   | Remove-AppxPackage
+Get-AppxPackage **   | Remove-AppxPackage
+
+
---
+++ remove_xbox.ps1
@@ -0,0 +1,8 @@
+Get-AppxPackage -allusers Microsoft.549981C3F5F10 | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage
+Get-AppxPackage *xboxapp* | Remove-AppxPackage
+Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxGameOverlay | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage
